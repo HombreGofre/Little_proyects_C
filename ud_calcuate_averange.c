2b16b805bd1bde0ddb640014556621cb133f3a8c
HombreGofre <c.andresns@hotmail.com>
2024-02-05 20:44:53 +0100

    First program to calculate the average completed

--- ud_calcuate_averange.c
+++ ud_calcuate_averange.c
@@ -2,24 +2,27 @@
 
 /*
  * This function averages the numbers we have and returns the result.
- * USA SIZE_T
  */
 
 int main()
 {
-	int	i;//contador de  poición y añadimos los datos
-	int	*n;//añadimos los datos 
-	int t;
-	float prom;
+	int	i;//contador de posición y cantidad
+	int	num; //añadimos los datos
+	int	*n;//añadimos los datos al array
+	int t; //Sumatorio de los datos
+	float prom; //Promedio de los datos
 
+	i = 0;
+	num = 0;
+	t = 0;
 	printf("Add the quantity of numbers:\n");
 	scanf("%d", &i);
 	printf("Add numbers:");
-	while (i-- > 0)
+	while (++num <= i)
 	{
-		scanf("%d", &n[i]);
-		t += n[i];
+		scanf("%d", &n[num]);
+		t += n[num];
 	}
 	prom = t/i;
-	printf("The averange of numbers is: %d", &prom);
+	printf("The averange of numbers is: %.2f", prom);
 }
