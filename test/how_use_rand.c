4c87178b9f2883aa87836d85c02ccf00c3785593
HombreGofre <c.andresns@hotmail.com>
2024-03-04 15:12:38 +0100

    Function even and odd

---
+++ test/how_use_rand.c
@@ -0,0 +1,13 @@
+#include <stdio.h>      
+#include <stdlib.h>  
+int main()  
+{     
+    // use rand() function to generate the number  
+    printf (" The random number is: %d", rand() % 100);  
+    printf ("\n The random number is: %d", rand() % 100);  
+      
+    printf (" \n The random number is: %d", rand() % 100);  
+    printf ("\n The random number is: %d", rand() % 100);  
+    getchar();
+   return 0;	
+}  
--- ud_even_odd.c
+++ ud_even_odd.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /*
  * Create a program that adds random numbers to an array of integers and 
@@ -13,13 +14,41 @@ int main()
 	int odd[100];
 	int i;
 	int	j;
+	int k;
+	time_t	t1;
 
+	srand((unsigned) time (&t1));
 	j = 0;
 	i = 0;
-	num[j] = rand() % 100;
+	k = 0;
 	while (j < 100)
 	{
-		printf("%d ", num[i]);
+		num[j] = rand() % 100;
+		if (num[j] % 2 == 0)
+		{
+			even[i] = num[j];
+			i++;
+		}
+		else if (num[j] % 2 != 0)
+		{
+			odd[k] = num[j];
+			k++;
+		}
+		printf("%d ", num[j]);
+		j++;
+	}
+	printf("\n************************************ \n");
+	i = 0;
+	while (even[i])
+	{
+		printf("%d ", even[i]);
 		i++;
+	}
+	printf("\n************************************ \n");
+	k = 0;
+	while (odd[k])
+	{
+		printf("%d ", odd[k]);
+		k++;
 	}
 }
